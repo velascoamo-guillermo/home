67a5c257107c7115b30d17e30803b7c5fc2c04ae
Guillermo Velasco <velascoamo.guillermo@gmail.com>
2026-05-15 17:20:47 +0200

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by Guillermo Velasco on 15/5/26.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
