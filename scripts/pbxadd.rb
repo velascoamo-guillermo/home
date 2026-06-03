2ba154bea6550149e82a6182c79334058f10211d
Guillermo Velasco <velascoamo.guillermo@gmail.com>
2026-06-03 20:55:56 +0200

    chore: add pbxproj add/remove helper scripts

---
+++ scripts/pbxadd.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# Usage: ruby scripts/pbxadd.rb <group/path> <Filename.swift> <TargetName>
+# Example: ruby scripts/pbxadd.rb Home/Stock Supermarket.swift Home
+require 'xcodeproj'
+
+group_path, filename, target_name = ARGV
+abort "usage: pbxadd.rb <group/path> <Filename.swift> <Target>" unless group_path && filename && target_name
+
+proj   = Xcodeproj::Project.open('Home.xcodeproj')
+target = proj.targets.find { |t| t.name == target_name } or abort "no target #{target_name}"
+
+group = proj.main_group
+group_path.split('/').each do |seg|
+  group = group[seg] || group.new_group(seg, seg)
+end
+
+if group.files.any? { |f| f.display_name == filename }
+  puts "already registered: #{filename}"
+else
+  ref = group.new_reference(filename)
+  target.add_file_references([ref])
+  puts "added #{group_path}/#{filename} to #{target_name}"
+end
+proj.save
---
+++ scripts/pbxremove.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# Usage: ruby scripts/pbxremove.rb <Filename.swift>
+require 'xcodeproj'
+
+filename = ARGV[0] or abort "usage: pbxremove.rb <Filename.swift>"
+proj = Xcodeproj::Project.open('Home.xcodeproj')
+
+proj.files.select { |f| f.display_name == filename }.each do |ref|
+  proj.targets.each do |t|
+    t.source_build_phase.files.select { |bf| bf.file_ref == ref }.each do |bf|
+      t.source_build_phase.remove_build_file(bf)
+    end
+  end
+  ref.remove_from_project
+  puts "removed #{filename}"
+end
+proj.save
